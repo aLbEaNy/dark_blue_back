c1eeb99626343e04180f7efcd73bf3ebef65171f
albeany <atabernerovalle@gmail.com>
2025-07-24 14:59:46 +0200

    Añadido readme

---
+++ src/main/java/personal/darkblueback/DarkBlueBackApplication.java
@@ -0,0 +1,14 @@
+package personal.darkblueback;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarkBlueBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DarkBlueBackApplication.class, args);
+    }
+
+}
