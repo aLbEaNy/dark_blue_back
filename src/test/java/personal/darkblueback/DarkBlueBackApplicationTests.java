2a81d76b168f5de531be415d92a26335dbb376b3
albeany <atabernerovalle@gmail.com>
2025-07-24 14:59:46 +0200

    Login y registro con security

---
+++ src/test/java/personal/darkblueback/DarkBlueBackApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.darkblueback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarkBlueBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
